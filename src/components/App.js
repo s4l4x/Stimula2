e2de54aacc48f1c1d053f2d383cb8fa58664b01b
Alessandro Sabatelli <planoform@gmail.com>
2023-05-28 18:58:14 +0200

    feat: Move audio player up above chrome

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,33 @@
 import { Canvas } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Perf } from 'r3f-perf'
 import { OrbitControls, CameraShake } from '@react-three/drei'
 import { Leva } from 'leva'
 import { useControls } from 'leva'
 import { Visualizer } from './Visualizer'
 import { AudioAnalyzer } from '../lib/audio-analyzer'
+import Audio from '../assets/oliver-20230516-stimula.wav'
+import './App.css'
 
 export default function App() {
-  const [audioUrl, setAudioUrl] = useState(null)
+  const [audioUrl, setAudioUrl] = useState(Audio)
   const [analyzer, setAnalyzer] = useState(null)
+  const [isAnalyzerReady, setIsAnalyzerReady] = useState(false)
   const audioElmRef = useRef(null)
 
-  const onFileChange = (e) => {
-    const file = e.target.files?.[0]
-    if (!file) return
-    setAudioUrl(URL.createObjectURL(file))
-    setAnalyzer(new AudioAnalyzer(audioElmRef.current))
-  }
+  useEffect(() => {
+    // Set CSS variable for 1vh for use when positioning elements in CSS above chrome
+    const vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  }, [])
+
+  // Load audio file
+  // const onFileChange = (e) => {
+  //   const file = e.target.files?.[0]
+  //   if (!file) return
+  //   setAudioUrl(URL.createObjectURL(file))
+  //   setAnalyzer(new AudioAnalyzer(audioElmRef.current))
+  // }
 
   const { perfVisible } = useControls('Debug', {
     perfVisible: false
@@ -35,7 +45,7 @@ export default function App() {
         }}
         style={{
           width: '100vw',
-          height: 'calc(100vh - 80px)',
+          height: '100vh',
           background: '#000'
         }}>
         {perfVisible && <Perf position="top-left" />}
@@ -45,17 +55,27 @@ export default function App() {
         <Visualizer analyzer={analyzer} />
       </Canvas>
       {/* Audio */}
-      <div
+      {/* Load audio file UI */}
+      {/* <div
         style={{
           height: 80,
           display: 'flex',
           justifyContent: 'space-around',
           alignItems: 'center'
-        }}>
-        <label htmlFor="file-upload" className="custom-file-upload">
+        }}> */}
+      {/* <label htmlFor="file-upload" className="custom-file-upload">
           Choose Audio File
           <input id="file-upload" type="file" accept="audio/*" onChange={onFileChange} />
-        </label>
+        </label>*/}
+      <div className="audio-player">
+        {!analyzer && (
+          <button
+            onClick={() => {
+              setAnalyzer(new AudioAnalyzer(audioElmRef.current))
+            }}>
+            Setup
+          </button>
+        )}
         <audio src={audioUrl} controls ref={audioElmRef} />
       </div>
     </>
